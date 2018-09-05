fdb9ec56c154315f7eb5e3128b26d6e926899c19
DonCuicui <alexandre.Quilan@gmail.com>
2018-09-05 17:22:05 +0200

    UN DEBUT MAIS QUAND MEME MIGNON

--- ClickMon/dualMonster/models.py
+++ ClickMon/dualMonster/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Attack(models.Model):
+    name = models.CharField(max_length=20)
+    samina_cost=models.IntegerField
+
+class Summoner(models.Model):
+    name = models.CharField(max_length=20)
+    mdp = models.CharField(max_length=20)
+    MALE = 'Mr'
+    FEMALE = 'Mme'
+    GENDER_SELECTION = (
+        (MALE, 'Garçon'),
+        (FEMALE, 'Fille'),
+    )
+    gender = models.CharField(max_length=20, choices=GENDER_SELECTION, default=MALE)
+    money = models.IntegerField(default=50)
+    exp = models.IntegerField(default=0)
+    def __str__(self):
+        return f'SALUT JE SUIS LE DRESSEUR DE CLICKMON {self.name}'
+
+
+class Clickmon(models.Model):
+    summoner = models.ForeignKey(Summoner, related_name='clickmon',
+                               on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    hp_max = models.IntegerField(default=0)
+    hp = models.IntegerField(default=0)
+    exp = models.IntegerField(default=0)
+    element = models.CharField(max_length=20)
+    img_front = models.ImageField(max_length=100)
+    img_back =models.ImageField(max_length=100)
+    stamina = models.IntegerField(default=0)
+    damage = models.IntegerField(default=0)
+    def __str__(self):
+        return f'SALUT JE SUIS LE CLICKMON {self.name}'
+
+
