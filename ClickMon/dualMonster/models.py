5eca6ff4651ce23b340550eec2a5920b949617ba
DonCuicui <alexandre.Quilan@gmail.com>
2018-09-06 09:49:15 +0200

    BDD complete

--- ClickMon/dualMonster/models.py
+++ ClickMon/dualMonster/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 class Attack(models.Model):
     name = models.CharField(max_length=20)
-    samina_cost=models.IntegerField
+    stamina_cost = models.IntegerField(default=0)
+    img =models.ImageField(max_length=100)
+    damage = models.IntegerField(default=0)
+    element = models.CharField(max_length=20)
+    def __str__(self):
+        return f'{self.name} attaque type : {self.element}'
 
 class Summoner(models.Model):
     name = models.CharField(max_length=20)
@@ -17,8 +22,7 @@ class Summoner(models.Model):
     money = models.IntegerField(default=50)
     exp = models.IntegerField(default=0)
     def __str__(self):
-        return f'SALUT JE SUIS LE DRESSEUR DE CLICKMON {self.name}'
-
+        return f'{self.name}'
 
 class Clickmon(models.Model):
     summoner = models.ForeignKey(Summoner, related_name='clickmon',
@@ -31,8 +35,21 @@ class Clickmon(models.Model):
     img_front = models.ImageField(max_length=100)
     img_back =models.ImageField(max_length=100)
     stamina = models.IntegerField(default=0)
+    stamina_max = models.IntegerField(default=0)
     damage = models.IntegerField(default=0)
     def __str__(self):
-        return f'SALUT JE SUIS LE CLICKMON {self.name}'
+        return f'{self.name} de {self.summoner}'
 
+class AttackPack(models.Model):
+    Clickmon = models.ForeignKey(Clickmon, related_name='clickmon_attack', on_delete=models.CASCADE)
+    attack_one = models.ForeignKey(Attack, related_name='attack_one_rn', on_delete=models.CASCADE)
+    exp_one = models.IntegerField(default=0)
+    attack_two = models.ForeignKey(Attack, related_name='attack_two_rn', on_delete=models.CASCADE)
+    exp_two = models.IntegerField(default=0)
+    attack_three = models.ForeignKey(Attack, related_name='attack_three_rn', on_delete=models.CASCADE)
+    exp_three = models.IntegerField(default=0)
+    attack_four = models.ForeignKey(Attack, related_name='attack_four_rn', on_delete=models.CASCADE)
+    exp_four = models.IntegerField(default=0)
+    def __str__(self):
+        return f'Attaques de {self.Clickmon}'
 
