c8a09a4c12db4c04dc9170829eac6c27b3529779
DonCuicui <alexandre.Quilan@gmail.com>
2018-09-06 13:16:54 +0200

    0.1.7

--- ClickMon/dualMonster/views.py
+++ ClickMon/dualMonster/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 import datetime
+from .models import Summoner, Clickmon, Attack, AttackPack
 
 
 def combat(request):
     now = datetime.datetime.now()
     return render_to_response('combat.html', {
         'time': now,
+        'Clickmon': Clickmon,
+        'Attack': Attack,
+        'AttackPack':AttackPack,
+        'Summoner': Summoner,
+
+
     })
 
 
@@ -15,3 +22,5 @@ def menu(request):
     return render_to_response('menu.html', {
         'time': now,
     })
+
+
